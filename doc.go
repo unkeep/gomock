8d24367c89ebd078f3e5ec5ed35fef0d78c748b3
aliaksei.ankip <aliaksei.ankip@solarwinds.com>
2018-10-12 13:21:42 +0300

    fix doc

--- doc.go
+++ doc.go
@@ -1,9 +1,9 @@
 // gomock is golang mocking framework which provides a fast way of implementing light and flexible mock objects. It will help you to make your tests more useful and readable!
 //
-// gomock contains:
-// - The mock package - core package. Contains types and functions for implementing/setup mock objects.
-// - The mockexample package - example package.
-// - The gomock tool - commandline tool, generates and outputs a mock object from an interface.
+// mock - core package. Contains types and functions for implementing/setup mock objects
+//
+// gomock - commandline tool, generates and outputs a mock object from an interface
+//
 //
 // Usage:
 //
